c2619772f7779150523a893a74c1092358c52795
LLlOPT <llloptman@gmail.com>
2020-10-14 17:55:45 +0300

    task 2 added PostManager

---
+++ src/main/java/manager/NotesManager.java
@@ -0,0 +1,16 @@
+package main.java.manager;
+
+import main.java.domain.NoteOnTheWall;
+
+public class NotesManager {
+    private NoteOnTheWall[] noteOnTheWalls;
+
+    public NoteOnTheWall[] search(int ownerId, String domain, String query, boolean ownersOnly, int count) {
+        //add some logic
+        return noteOnTheWalls;
+    }
+
+    public void delete(int ownerId, int postId) {
+        //add some logic
+    }
+}
